ecc4067012de5d83519ffaed00eaab6e3d8ee949
shalini-lodhi <shalinilodhi01@gmail.com>
2023-03-23 21:44:51 +0530

    1_Event Call Back
    using [parameter] in _individualComponent.razor for event call in "Favorite product count"

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
+        public List<Demo_ProductProp> Prop { get; set; }
+    }
+}
